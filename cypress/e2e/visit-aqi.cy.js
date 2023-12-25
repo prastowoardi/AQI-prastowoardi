22abb959fe862070c78b20882bdf02c56ded27fa
prastowoardi <prastowoardi616@gmail.com>
2023-12-25 18:12:13 +0700

    cek bahasa informasi pengaturan cookie

--- cypress/e2e/visit-aqi.cy.js
+++ cypress/e2e/visit-aqi.cy.js
@@ -1,5 +1,21 @@
 describe('template spec', () => {
-  it('Visit AQI website', () => {
+  afterEach ('Screenshoot ketika tes gagal',function (){
+    if (this.currentTest.state === "failed") {
+        const testName = this.currentTest.title.replace(/\s+/g, '-')
+        cy.log('TEST FAILED')
+        cy.screenshot(testName, { capture: 'runner' })
+    }
+  })
+
+  it('Membaca teks bahasa inggris jika pada URL terdapat "/en/"', () => {
     cy.visit('')
+    cy.url().should('eq', 'https://www.asia-quest.jp/en/')
+    cy.url().should('include', '/en/')
+
+    cy.get('#hs-eu-policy-wording').invoke('text').then((text) => {
+      if (!Cypress.config().baseUrl.includes('/en/') && !text.toLowerCase().includes('setting cookie')) {
+        throw new Error('Teks pada pengaturan cookie bukan dalam bahasa Inggris')
+      }
+    })
   })
 })
